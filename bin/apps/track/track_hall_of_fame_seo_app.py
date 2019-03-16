ea4e24fa95812562c946271c51d98fbd0c1d122b
Christian Ochoa <calochoa@gmail.com>
2019-03-16 01:20:57 -0700

    add breadcrumb to hall of fame; update homepage and track homepage links

--- bin/apps/track/track_hall_of_fame_seo_app.py
+++ bin/apps/track/track_hall_of_fame_seo_app.py
@@ -21,6 +21,10 @@ def get_track_hof_field_results(event_id, gender_id):
 
 
 
+@track_hall_of_fame_seo_app.route('/',methods=['GET'])
+def track_hof_home():
+    return render_template('track/hall-of-fame/hallOfFame.html')
+
 # Sprints
 @track_hall_of_fame_seo_app.route('/boys-100m-dash/',methods=['GET'])
 def track_boys_100m_dash():
